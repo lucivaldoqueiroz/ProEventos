c0398260edeb62d49aaaa0d695c0be31cccf25c1
Lucivaldo Queiroz <lucivaldo.npqueiroz@gmail.com>
2024-01-10 21:09:48 -0300

    componentes e diretiva ng for

---
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-eventos',
+  templateUrl: './eventos.component.html',
+  styleUrls: ['./eventos.component.scss']
+})
+export class EventosComponent implements OnInit {
+
+  public eventos: any =  [{
+    Tema: 'Angular 11',
+    Local: 'Belo Horizonte'
+  },
+  {
+    Tema: 'Dotnet5',
+    Local: 'São Paulo',
+  },
+  {
+    Tema: 'Angular e suas novidades',
+    Local: 'Rio de Janeiro',
+  },
+]
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
